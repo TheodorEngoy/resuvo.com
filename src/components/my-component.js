0de6d98d322b99521e8e337b88772b83e5e7e545
theodorengoy <engoy.theodor@gmail.com>
2024-06-22 14:38:29 +0200

    test commit

--- src/components/my-component.js
+++ src/components/my-component.js
@@ -1,7 +1,7 @@
 import { ref } from 'vue'
 export default {
     setup() {
-        const count = ref(0)
+        const count = ref(1)
         return { count }
     },
     template: `<div>Count is: {{ count }}</div>`
